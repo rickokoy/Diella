19e65926d8215f14a383886a740f89856e26f2e0
rickokoy <96056671+rickokoy@users.noreply.github.com>
2024-10-31 00:42:49 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -14,6 +14,7 @@ window.addEventListener('load', () => {
       document.querySelector('.song').play();
       resolveFetch().then(animationTimeline());
     } else {
+      document.querySelector('.song').play();
       resolveFetch().then(animationTimeline());
     }
   })
